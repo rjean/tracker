eb184ee177d0513ae0b072a121c2567bacca8934
Raphael Jean <raphael.jean@rocketmail.com>
2020-05-17 15:06:42 +0100

    Embedded video in UI.

--- www_symlinks.sh
+++ www_symlinks.sh
@@ -1,4 +1,5 @@
 # Creates symbolic links for the NGINX Web Server on the Raspbeery PI
 # for the files in the www folder.
 sudo ln -s /home/pi/follower/www/ui.html /var/www/html/ui.html
-
+sudo ln -s /home/pi/follower/www/camera.html /var/www/html/camera.html
+sudo ln -s /home/pi/follower/www/jsmpg.js /var/www/html/jsmpg.js
