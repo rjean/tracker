8acccd68c6419de116036ebe29bf4449e74e0513
Raphael <raphael.jean@rocketmail.com>
2020-09-26 15:24:22 +0100

    SImplified the stack.

--- rtcom.py
+++ rtcom.py
@@ -3,16 +3,58 @@ import threading
 import yaml
 import socket
 import time
+import re
+
+def read_raw_message(data):
+    for i in range(len(data)):
+        if data[i]==ord('\n'):
+            return data[0:i].decode("utf-8"), data[i+1:]
+    raise ValueError("Unable to find the end of line")
+
+def read_message(data):
+    header, data = read_raw_message(data)
+    #'device/endpoint:encoding'
+    match = re.search("(.*?)/(.*?):(.*)", header)
+    device = match[1]
+    endpoint = match[2]
+    encoding = match[3]
+    if encoding=="yaml":
+        data = yaml.load(data.decode("utf-8"))
+    return device, endpoint, data, encoding
+
+def build_message(device_name, endpoint, data, encoding):
+        header = bytes(f"{device_name}/{endpoint}:{encoding}\n", "utf-8")
+        if encoding=="yaml":
+            encoded_data = bytes(yaml.dump(data), "utf-8")
+        elif encoding=="binary":
+            encoded_data = data
+        else:
+            raise ValueError("Please specify a supported encoding methods. (binary or yaml)")
+
+        size = len(header) + len(encoded_data)
+        message = bytearray(size)
+        message[0:len(header)]=header
+        message[len(header):]=encoded_data
+        return message
 
 class Device():
     def __init__(self, name, addr):
-        threading.Thread.__init__(self)
         self.name=name
         self.addr=addr
         self.broadcast = False
         self.enabled = True
         #self.rtcom = rtcom
-        self.messages = {}
+        self.endpoints = {}
+    
+    def __getitem__(self, key):
+        return self.endpoints[key].data
+
+class Endpoint():
+    def __init__(self, name, encoding, data = None):
+        self.name = name
+        self.encoding = encoding
+        self.data = data
+
     
 
 class RealTimeCommunication:
@@ -33,6 +75,9 @@ class RealTimeCommunication:
         self.listen_thread.start()
         self.devices = {}
 
+    def __getitem__(self, key):
+        return self.listen_thread.devices[key]
+
     def __enter__(self):
         return self
         
@@ -42,14 +87,18 @@ class RealTimeCommunication:
                          "endpoints" : self.endpoints}}
         self.broadcast(message)
 
-    def broadcast(self, message, port=5999):
+    def broadcast(self, data, port=5999):
         sock = socket.socket(socket.AF_INET, # Internet
                              socket.SOCK_DGRAM) # UDP
-        encoded_message = yaml.dump(message)
-        data = encoded_message.encode('utf-8')
+        #encoded_message = yaml.dump(message)
+        #data = encoded_message.encode('utf-8')
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         sock.sendto(data, ("255.255.255.255", port))
         sock.close()
+
+    def broadcast_endpoint(self, endpoint, data, encoding="yaml"):
+        message = build_message(self.device_name, endpoint, data, encoding)
+        self.broadcast(message)
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.listen_thread.enabled=False
@@ -76,14 +125,23 @@ class RealTimeCommunicationListener(threading.Thread):
         while self.enabled:
             try:
                 data, addr = self.sock.recvfrom(1024) # buffer size is 1024 bytes
-                data_dict = yaml.load(data, Loader=yaml.Loader)
-                self.data_dict = data_dict
-                if "announce" in data_dict:
-                    device_name = data_dict["announce"]["device_name"]
-                    if device_name not in self.devices:
-                        self.devices[device_name] = Device(device_name, addr)                        
+                device, endpoint, data, encoding = read_message(data)
+                if device not in self.devices:
+                    self.devices[device] = Device(device, addr)
+                
+                if endpoint not in self.devices[device].endpoints:
+                    self.devices[device].endpoints[endpoint] = Endpoint(endpoint, encoding, data)
+                else:
+                    self.devices[device].endpoints[endpoint].data = data
+                    
+                # data_dict = yaml.load(data, Loader=yaml.Loader)
+                # self.data_dict = data_dict
+                # if "announce" in data_dict:
+                #    device_name = data_dict["announce"]["device_name"]
+                #    if device_name not in self.devices:
+                                              
                 self.miss_counter=0
-                print("received message: %s" % self.data_dict)
+                print(f"{device}, {endpoint}, {encoding}, {len(data)}")
             except:
                 self.miss_counter+=1
         self.sock.close()
--- rtcom_test.py
+++ rtcom_test.py
@@ -1,7 +1,7 @@
 #Real-time communications
 import unittest
 #import thread
-from rtcom import RealTimeCommunication
+from rtcom import *
 
 from time import sleep
 #Test for coordinates transformations. 
@@ -9,27 +9,6 @@ class TestRealTimeCommunication(unittest.TestCase):
     def test_test(self):
         self.assertTrue(True)
         
-    def test_broadcast(self):
-        with RealTimeCommunication() as rtcom:
-            test_message = {"Hello" : "World"}
-            rtcom.broadcast(test_message) 
-            sleep(0.1)
-            self.assertEqual(rtcom.listen_thread.data_dict,test_message)
-
-
-    def test_announce(self):
-        with RealTimeCommunication("test_device") as rtcom:
-            rtcom.announce()
-            sleep(0.2)
-            self.assertTrue("test_device" in rtcom.listen_thread.devices)
-
-    def test_announce_turret(self):
-        with RealTimeCommunication("turret.local") as rtcom:
-            rtcom.announce()
-            sleep(0.1)
-            self.assertTrue("turret.local" in rtcom.listen_thread.devices)
- 
-
     def test_timeout(self):
         with RealTimeCommunication() as rtcom:
             sleep(1)
@@ -37,10 +16,37 @@ class TestRealTimeCommunication(unittest.TestCase):
 
     def test_device_message(self):
         with RealTimeCommunication("turret.local") as rtcom:
-            rtcom.announce()
-            sleep(1) 
-        x=1
+            rtcom.broadcast_endpoint("heartbeat", 10)
+            sleep(0.1) 
+            self.assertEqual(rtcom.listen_thread.devices["turret.local"].endpoints["heartbeat"].data, 10)
+            self.assertEqual(rtcom["turret.local"]["heartbeat"],10)
+            rtcom.broadcast_endpoint("heartbeat", 11)
+            sleep(0.1) 
+            self.assertEqual(rtcom.listen_thread.devices["turret.local"].endpoints["heartbeat"].data, 11)
+            self.assertEqual(rtcom["turret.local"]["heartbeat"],11)
+            rtcom.broadcast_endpoint("binary_data", bytes([1,2,3,4]), encoding="binary")
+            sleep(0.1) 
+            self.assertEqual(rtcom["turret.local"]["binary_data"],bytes([1,2,3,4]))
         
+    def test_build_message_yaml(self):
+        message = build_message("device", "endpoint", {"hello" : "world"}, "yaml")
+        self.assertEqual(len(message),34)
+        self.assertIsInstance(message, bytearray)
+
+    def test_read_raw_message(self):
+        message = build_message("device", "endpoint", {"hello" : "world"}, "yaml")
+        header_line, data = read_raw_message(message)
+        self.assertEqual(header_line,"device/endpoint:yaml")
+        self.assertIsInstance(data, bytearray)
+        self.assertEqual(len(data),13)
+
+    def test_read_message(self):
+        message = build_message("device", "endpoint", {"hello" : "world"}, "yaml")
+        device, endpoint, data, encoding = read_message(message)
+        self.assertEqual(device, "device")
+        self.assertEqual(endpoint, "endpoint")
+        self.assertEqual(encoding,"yaml")
+        self.assertEqual(data, {"hello" : "world"})
 
 
 
