5b827bcee07ef65fb49cb474847f54b29871121b
Raphael <raphael.jean@rocketmail.com>
2020-09-26 13:50:22 +0100

    Refactor + Use with statement,

--- rtcom.py
+++ rtcom.py
@@ -1,8 +1,19 @@
 import sched
 import threading
-import json
+import yaml
 import socket
 import time
+
+class Device():
+    def __init__(self, name, addr):
+        threading.Thread.__init__(self)
+        self.name=name
+        self.addr=addr
+        self.broadcast = False
+        self.enabled = True
+        #self.rtcom = rtcom
+        self.messages = {}
+    
 
 class RealTimeCommunication:
     def __init__(self, device_name=None):
@@ -10,13 +21,21 @@ class RealTimeCommunication:
             self.device_name = socket.gethostname()
         else:
             self.device_name = device_name
+        try:
+            addr = socket.gethostbyname(self.device_name)
+        except:
+            addr = "127.0.0.1"
+
+        self.this_device = Device(self.device_name, addr)
 
         self.endpoints = {}
-        self.listen_thread = self.Listen(self)
+        self.listen_thread = RealTimeCommunicationListener(self)
         self.listen_thread.start()
         self.devices = {}
-        
 
+    def __enter__(self):
+        return self
+        
     def announce(self):
         message = {"announce" : 
                         {"device_name": self.device_name, 
@@ -26,49 +45,45 @@ class RealTimeCommunication:
     def broadcast(self, message, port=5999):
         sock = socket.socket(socket.AF_INET, # Internet
                              socket.SOCK_DGRAM) # UDP
-        json_message = json.dumps(message)
-        data = json_message.encode('utf-8')
+        encoded_message = yaml.dump(message)
+        data = encoded_message.encode('utf-8')
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         sock.sendto(data, ("255.255.255.255", port))
         sock.close()
 
-    def stop(self):
+    def __exit__(self, exc_type, exc_value, traceback):
         self.listen_thread.enabled=False
         self.listen_thread.join()
 
-    class Listen(threading.Thread):
-        def __init__(self, rtcom, port=5999, hostname=None):
-            threading.Thread.__init__(self)
-            if hostname is None:
-                UDP_IP = "0.0.0.0"
-            self.sock = socket.socket(socket.AF_INET, # Internet
-                                 socket.SOCK_DGRAM) # UDP
-            self.sock.bind((UDP_IP, port))
-            self.sock.settimeout(0.1)
-            self.json_data=None
-            self.enabled=True
-            self.miss_counter=0
-            self.rtcom = rtcom
 
+class RealTimeCommunicationListener(threading.Thread):
+    def __init__(self, rtcom, port=5999, hostname=None):
+        threading.Thread.__init__(self)
+        if hostname is None:
+            UDP_IP = "0.0.0.0"
+        print(f"Listening to {UDP_IP}")
+        self.sock = socket.socket(socket.AF_INET, # Internet
+                             socket.SOCK_DGRAM) # UDP
+        self.sock.bind((UDP_IP, port))
+        self.sock.settimeout(0.1)
+        self.data_dict=None
+        self.enabled=True
+        self.miss_counter=0
+        self.rtcom = rtcom
+        self.devices = {}
 
-        def run(self):
-            while self.enabled:
-                try:
-                    data, addr = self.sock.recvfrom(1024) # buffer size is 1024 bytes
-                    json_data = json.loads(data)
-                    self.json_data = json_data
-                    if "announce" in json_data:
-                        device_name = json_data["announce"]["device_name"]
-                        self.rtcom.devices[device_name] = json_data["announce"]["endpoints"]
-                        
-                    self.miss_counter=0
-                    print("received message: %s" % self.json_data)
-                except:
-                    self.miss_counter+=1
-            self.sock.close()
-                #print(addr)
-
-#rtcom = RealTimeCommunication()
-#while True:
-#    rtcom.broadcast({"Hello" :"World!"})
-
+    def run(self):
+        while self.enabled:
+            try:
+                data, addr = self.sock.recvfrom(1024) # buffer size is 1024 bytes
+                data_dict = yaml.load(data, Loader=yaml.Loader)
+                self.data_dict = data_dict
+                if "announce" in data_dict:
+                    device_name = data_dict["announce"]["device_name"]
+                    if device_name not in self.devices:
+                        self.devices[device_name] = Device(device_name, addr)                        
+                self.miss_counter=0
+                print("received message: %s" % self.data_dict)
+            except:
+                self.miss_counter+=1
+        self.sock.close()
--- rtcom_test.py
+++ rtcom_test.py
@@ -10,26 +10,39 @@ class TestRealTimeCommunication(unittest.TestCase):
         self.assertTrue(True)
         
     def test_broadcast(self):
-        rtcom = RealTimeCommunication()
-        test_message = {"Hello" : "World"}
-        rtcom.broadcast(test_message) 
-        sleep(0.1)
-        self.assertEqual(rtcom.listen_thread.json_data,test_message)
-        rtcom.stop()
+        with RealTimeCommunication() as rtcom:
+            test_message = {"Hello" : "World"}
+            rtcom.broadcast(test_message) 
+            sleep(0.1)
+            self.assertEqual(rtcom.listen_thread.data_dict,test_message)
 
 
     def test_announce(self):
-        rtcom = RealTimeCommunication("test_device")
-        rtcom.announce()
-        sleep(0.1)
-        self.assertTrue("test_device" in rtcom.devices)
-        rtcom.stop()
+        with RealTimeCommunication("test_device") as rtcom:
+            rtcom.announce()
+            sleep(0.2)
+            self.assertTrue("test_device" in rtcom.listen_thread.devices)
+
+    def test_announce_turret(self):
+        with RealTimeCommunication("turret.local") as rtcom:
+            rtcom.announce()
+            sleep(0.1)
+            self.assertTrue("turret.local" in rtcom.listen_thread.devices)
+ 
 
     def test_timeout(self):
-        rtcom = RealTimeCommunication()
-        sleep(1)
-        self.assertEqual(rtcom.listen_thread.miss_counter,9)
-        rtcom.stop() 
+        with RealTimeCommunication() as rtcom:
+            sleep(1)
+            self.assertEqual(rtcom.listen_thread.miss_counter,9)
+
+    def test_device_message(self):
+        with RealTimeCommunication("turret.local") as rtcom:
+            rtcom.announce()
+            sleep(1) 
+        x=1
+        
+
+
 
 if __name__ == '__main__':
     unittest.main()
